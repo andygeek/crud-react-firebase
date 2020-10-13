fce285c41d8c0a5e94da5483261b523c1d55a120
AndyGeek <andy.eulogio@gmail.com>
2020-10-13 15:23:18 -0500

    Fix errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { firebase } from "./firebase";
 import "./App.css";
 
 export const App = () => {
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const db = firebase.firestore();
+        const data = await db.collection("tasks").get();
+        console.log(data.docs);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Crud React Firebase</h1>
