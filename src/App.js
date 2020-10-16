34fd98b85c2f6a3e129e88d64f18f3cfeee8cba1
AndyGeek <andy.eulogio@gmail.com>
2020-10-16 11:35:31 -0500

    Add useGetFirestore hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React, { useEffect } from "react";
-import { firebase } from "./firebase";
+import React from "react";
+import { useGetFirestore } from "./hooks/useGetFirestore";
 import "./App.css";
 
 export const App = () => {
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const db = firebase.firestore();
-        const data = await db.collection("tasks").get();
-        console.log(data.docs);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getData();
-  }, []);
+  const { response, error, loading } = useGetFirestore("tasks");
 
   return (
     <div className="App">
       <h1>Crud React Firebase</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>Ahy un error</p>}
+      {response && <p>{response[0].nam}</p>}
     </div>
   );
 };
---
+++ src/hooks/useGetFirestore.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { firebase } from "../firebase";
+
+export const useGetFirestore = (collection) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const doFetch = async () => {
+      setLoading(true);
+      try {
+        const db = firebase.firestore();
+        const data = await db.collection(collection).get();
+        const arrayData = await data.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setResponse(arrayData);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    doFetch();
+  }, [collection]);
+  return {
+    response,
+    error,
+    loading,
+  };
+};
